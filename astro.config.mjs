b93ca11046bb2a58b74e8923c6dbff4e763bd5f3
morihoumatu <houmatuk@gmail.com>
2025-03-05 01:27:13 +0900

    Fix GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://kinova-llc.github.io/kinova-programming-blog/',
-  base: '/kinova-programming-blog',
-  integrations: [tailwind()]
+  site: "https://kinova-llc.github.io/kinova-programming-blog/",
+  base: "/kinova-programming-blog/",
+  integrations: [tailwind()], // 修正：関数として実行
+  output: "static",
+  build: {
+    outDir: "docs",
+  },
 });
