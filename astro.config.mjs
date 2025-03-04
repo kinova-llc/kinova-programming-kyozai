d0df94f9be8a127dcdf9f6fd5f773191676e9f7c
morihoumatu <houmatuk@gmail.com>
2025-03-05 01:16:18 +0900

    Setup GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kinova-llc.github.io',
+  site: 'https://kinova-llc.github.io/kinova-programming-blog/',
   base: '/kinova-programming-blog',
   integrations: [tailwind()]
 });
